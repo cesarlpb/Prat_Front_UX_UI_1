4db23b5976ef2457839f346ff972bae4244e58bd
LuisCallejaCaballero <tepeleno2002@hotmail.es>
2025-05-15 17:09:46 +0200

    descanso y continuo

---
+++ entregables/appfpsMD/performanceYmemo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FPSMeter from './components/FPSMeter';
+
+   function App() {
+      return (
+       <>
+          <h1>Demo FPS</h1>
+           <FPSMeter />
+         </>
+      );
+   }
+ 
+ export default App;
+  
+
+
